7915a7f8b02278f4a4c0e4c590c206a3098e9012
zhang.L <erzzhang@163.com>
2019-04-02 21:07:46 +0800

    13题C++

--- jianzhioffer/13.reOrderArray.cpp
+++ jianzhioffer/13.reOrderArray.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 
 /**
  * 《调整数组顺序使奇数位于偶数前面》
@@ -14,4 +15,25 @@
  * 2、使用deque,双端队列（deque，全名double-ended queue）是一种具有队列和栈性质的抽象数据类型。 
  * 双端队列中的元素可以从两端弹出，插入和删除操作限定在队列的两邊进行。
  * 方法有pop(), popleft(), append(), appendleft()
+ * #include <deque>
 */
+
+class Solution {
+public:
+    void reOrderArray(vector<int> &array) {
+        deque<int> result; 
+        int length = array.size();
+        for(int i=0; i < length; i++){
+            if((array[length-i-1]&1) == 1){ //奇数
+                result.push_front(array[length-i-1]);
+            }
+            if((array[i]&1) == 0){ //偶数
+                result.push_back(array[i]);
+            }            
+        }
+        // 因为此函数要求修改原数组而不是返回新数组，因此将原数组改掉
+        for(int j = 0;j<length; j++){
+            array[j] = result[j];
+        }
+    }
+};
